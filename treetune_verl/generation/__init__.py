f4fee7ca7131632972a305da013009b11c825680
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2026-02-04 01:38:37 -0500

    feat(generation): add public exports to __init__.py

--- treetune_verl/generation/__init__.py
+++ treetune_verl/generation/__init__.py
@@ -1,2 +1,17 @@
 # Copyright 2025 Treetune Authors
 # Licensed under the Apache License, Version 2.0
+"""Generation runner module for trajectory collection without training."""
+
+from treetune_verl.generation.checkpoint import CheckpointManager
+from treetune_verl.generation.config import GenerationConfig
+from treetune_verl.generation.queue import ResultsQueue
+from treetune_verl.generation.runner import GenerationRunner
+from treetune_verl.generation.worker import CollectorAgentLoopWorker
+
+__all__ = [
+    "GenerationRunner",
+    "GenerationConfig",
+    "ResultsQueue",
+    "CollectorAgentLoopWorker",
+    "CheckpointManager",
+]
